06010fdae1f774103772a5a293086cecc7c6c90c
wishtoday238 <wishtoday238@gmail.com>
2025-04-27 17:33:32 +0800

    挖矿方块添加右键增大挖掘范围功能

--- src/main/java/com/wishtoday/ts/Block/MiningOreBlock.java
+++ src/main/java/com/wishtoday/ts/Block/MiningOreBlock.java
@@ -9,9 +9,16 @@ import net.minecraft.block.BlockWithEntity;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityTicker;
 import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.registry.DynamicRegistryManager;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.Optional;
 
 public class MiningOreBlock extends BlockWithEntity {
     public MiningOreBlock(Settings settings) {
@@ -25,7 +32,7 @@ public class MiningOreBlock extends BlockWithEntity {
 
     @Override
     public @Nullable BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
-        return new  MiningOreBlockEntity(pos, state);
+        return new MiningOreBlockEntity(pos, state);
     }
 
     @Override
@@ -35,6 +42,23 @@ public class MiningOreBlock extends BlockWithEntity {
 
     @Override
     public @Nullable <T extends BlockEntity> BlockEntityTicker<T> getTicker(World world, BlockState state, BlockEntityType<T> type) {
-        return validateTicker(type,BlockEntityTypeRegister.MINING_ORE_BLOCK_ENTITY,MiningOreBlockEntity::tick);
+        return validateTicker(type, BlockEntityTypeRegister.MINING_ORE_BLOCK_ENTITY, MiningOreBlockEntity::tick);
+    }
+
+    @Override
+    protected ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, BlockHitResult hit) {
+        if (world.isClient) return super.onUse(state, world, pos, player, hit);
+        DynamicRegistryManager registryManager = world.getRegistryManager();
+        BlockEntity blockEntity = world.getBlockEntity(pos);
+        MiningOreBlockEntity entity = (MiningOreBlockEntity) blockEntity;
+        NbtCompound nbt = Optional.of(entity.createNbt(registryManager)).orElse(new NbtCompound());
+        if (!player.isSneaking()) {
+            nbt.putInt("SpreadDistance", entity.getSpreadDistance() + 1);
+            entity.read(nbt, registryManager);
+        } else {
+            nbt.putInt("SpreadDistance", entity.getSpreadDistance() - 1);
+            entity.read(nbt, registryManager);
+        }
+        return super.onUse(state, world, pos, player, hit);
     }
 }
